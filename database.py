a89400cd770a00d4c83c385e33a7c8a774bc6c3c
kazumakato-walls <107688753+kazumakato-walls@users.noreply.github.com>
2024-05-01 15:11:29 +0900

    first commit

---
+++ database.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# DBの準備。非同期処理の場合はsqlite+aiosqlite:でURLを記述
+ASYNC_DB_URL = 'sqlite+aiosqlite:///db.sqlite3'
+
+async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+async_session = sessionmaker(
+    autocommit=False, autoflush=False, bind=async_engine, class_ = AsyncSession
+)
+# DBの宣言
+Base = declarative_base()
+# セッションを取得し、DBへアクセス
+async def get_db():
+    async with async_session() as session:
+        yield session
